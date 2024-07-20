3073c8538047788d3c3e0eefbc26ff602d4aa753
vvm2003vvm <vvm2003vvm@example.com>
2024-07-20 20:34:58 +0530

    uploading question community pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,9 @@ import {
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+import "../styles/prism.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
